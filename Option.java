ba2254c50dfc8cf591765922b6e58007cdd2c794
Tracy Chen <1210tracychen@gmail.com>
2026-01-14 10:30:00 -0500

    Create Option class with description and consequence

---
+++ Option.java
@@ -0,0 +1,18 @@
+class Option
+{
+    String description;
+    Consequence consequence;
+    
+    public Option(String d, Consequence c){
+        description = d;
+        consequence = c;
+    }
+    
+    public int getHPChange(){
+        return consequence.consequence_HP;
+    }
+    
+    public int getAggresivenessChange(){
+        return consequence.consequence_aggresiveness;
+    }
+}
