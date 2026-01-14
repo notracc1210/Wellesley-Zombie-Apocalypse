0344b0ebed03df1e7832afb69c9ddfcbdba149e3
Tracy Chen <1210tracychen@gmail.com>
2026-01-14 10:31:32 -0500

    Add Consequence class with attributes and constructor

---
+++ Consequence.java
@@ -0,0 +1,11 @@
+class Consequence
+{
+    String description;
+    int consequence_HP,consequence_aggresiveness;
+    
+    public Consequence(String d, int h, int a){
+        description = d;
+        consequence_HP = h;
+        consequence_aggresiveness = a;
+    }
+}
