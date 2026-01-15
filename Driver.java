616e705affd10124fe9f3840ff2c8737cc212d86
Your Name <yourGithubAcctEmailAddress@example.com>
2026-01-15 08:17:28 -0500

    Created Driver class with instance variables

---
+++ Driver.java
@@ -0,0 +1,16 @@
+public class Driver{
+    private int hp;
+    private int aggressiveness;
+    private boolean hasRebirthToken; //acquired from roulette sucess
+    private boolean rebirthUsed; //can rebirth once
+    private boolean forceZombieNextRoll; //set if roulette fails
+    private GameMap map;
+    private Position currentPosition;
+    private Random rng;
+    private static final double CAREER_CENTER_SUCCESS_RATE = 0.10;
+
+    public static void main(String[] args){
+        System.out.println("Welleslsey Zombie Apocalypse")
+    }
+
+}
