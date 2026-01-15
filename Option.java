74354144cc2030cb08e871141cfe1740535ed325
Your Name <yourGithubAcctEmailAddress@example.com>
2026-01-15 13:26:00 -0500

    Added isSucessful method that evaluates probability of user sucess.

--- Option.java
+++ Option.java
@@ -2,16 +2,31 @@ class Option
 {
     String description;
     Consequence consequence;
-    
-    public Option(String d, Consequence c){
+    double sucessRate;
+    Random rand;
+
+    public Option(String d, Consequence c, double sucessRate){
         description = d;
         consequence = c;
+        sucessRate = sucessRate;
+        rand = new Random;
+
     }
-    
+
+    public boolean isSuccessful(){
+        double result = Math.random() +.01;
+        if(result <= sucessRate){
+            return true;
+        else{
+            return false;
+        }
+        }
+    }
+
     public int getHPChange(){
         return consequence.consequence_HP;
     }
-    
+
     public int getAggresivenessChange(){
         return consequence.consequence_aggresiveness;
     }
